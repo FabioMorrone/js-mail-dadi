ea5f8ab3632ef11eb5cff4897f3f744d4cd1b3f9
FabioMorrone <morronefabio04@gmail.com>
2025-02-13 15:57:46 +0100

    add Dadi

---
+++ Dadi/main.js
@@ -0,0 +1,7 @@
+/* 
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Prima di partire a scrivere codice poniamoci qualche domanda per individuare gli strumenti necessari allo sviluppo.
+*/
+
+const numero_giocatore = Math.round() * 6;
+const numero_pc = Math.round() * 6;
