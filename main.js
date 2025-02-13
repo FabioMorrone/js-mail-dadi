2e4e0923242a8245e0d3bd179b3da53284b5df56
FabioMorrone <morronefabio04@gmail.com>
2025-02-13 15:14:18 +0100

    add

--- main.js
+++ main.js
@@ -10,5 +10,8 @@ stampa un messaggio appropriato sull’esito del controllo.
 const invitedEmails = ["giovanni@gmail.com", "paolo@gmail.com", "giuseppe@gmail.com", "mario@gmail.com", "carmelo@gmail.com"]
 console.log(invitedEmails);
 
+/* chiedere di inserire la mail dell'utente */
+const theMail = prompt("Inserire mail dell'invitato")
 
 
+/* controllare se la mail è nella lista */
